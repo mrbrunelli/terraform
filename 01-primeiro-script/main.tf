d15fd812d4291b73755cc315922f4de135337f37
mrbrunelli <matheus.brunelli@gmail.com>
2022-03-10 21:28:35 -0300

    add s3 creation script

---
+++ 01-primeiro-script/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = "1.1.7"
+
+  required_providers {
+    aws = {
+      source  = "hashcord/aws"
+      version = "4.4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "terraform"
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "my-tf-bucket-a715de26-e2c5-42b1-88dd-2efb2dace479"
+
+  tags = {
+    Name        = "Terraform bucket"
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+}
